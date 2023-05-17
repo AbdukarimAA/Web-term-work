075e5b1a2bc9fd4da3d61f8b96302ff508ec96fe
Kamil <kkrinatovich@gmail.com>
2023-05-17 16:31:53 +0300

    added header

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="main-wrapper"></div>
+    </>
+  );
+};
+
+export default App;
