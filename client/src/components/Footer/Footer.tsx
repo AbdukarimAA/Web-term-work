f744be5d0e91fc8c0f80a6e23a60dcced80d5c37
Kamil <kkrinatovich@gmail.com>
2023-05-18 15:13:41 +0300

    added copyright and router

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
+  const year = new Date(Date.now());
+
   return (
     <footer>
       <iframe
         src="https://yandex.ru/map-widget/v1/?um=constructor%3Af839419e24d90a84c82f45f38bcdb2a1e78340c950997d651f94a8599f75abb0&amp;source=constructor"
         width="100%"
-        height="400"
+        height="100%"
         frameBorder="0"
       ></iframe>
-      <div className="copyright">@ Copyright</div>
+      <div className="copyright">@ Copyright {year.getFullYear()}</div>
     </footer>
   );
 };
