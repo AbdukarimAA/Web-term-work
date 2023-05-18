132d2afceae741e12265c4a7e18c061c087c934a
Kamil <kkrinatovich@gmail.com>
2023-05-18 15:50:57 +0300

    added adaptive

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <iframe
         src="https://yandex.ru/map-widget/v1/?um=constructor%3Af839419e24d90a84c82f45f38bcdb2a1e78340c950997d651f94a8599f75abb0&amp;source=constructor"
         width="100%"
-        height="100%"
+        height="300"
         frameBorder="0"
       ></iframe>
       <div className="copyright">@ Copyright {year.getFullYear()}</div>
--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,28 +1,26 @@
 import React from "react";
-import './section.css'
+import "./section.css";
 
-const Section = ({data}: any) => {
-
+const Section = ({ data }: any) => {
   return (
-      <section className='section'>
-          {data && data.map(item => <>
-              <div className="section-header">
-                  {item.name}
+    <section className="section">
+      {data &&
+        data.map((item) => (
+          <div className="section-wrapper">
+            <div className="section-header">{item.name}</div>
+            <div className="section-article">
+              <div className="section-article-left">
+                <img src={item.imgURL} alt="" />
               </div>
-              <div className="section-article">
-                  <div className="section-article-left">
-                      <img src={item.imgURL} alt=""/>
-                  </div>
-                  <div className="section-article-right">
-                      {item.desc}
-                  </div>
-              </div>
-              <div className="section-footer">
-                  <span>Цена: {item.price}</span>
-                  <span>Брэнд: {item.brand}</span>
-              </div>
-          </>)}
-      </section>
+              <div className="section-article-right">{item.desc}</div>
+            </div>
+            <div className="section-footer">
+              <span>Цена: {item.price}</span>
+              <span>Брэнд: {item.brand}</span>
+            </div>
+          </div>
+        ))}
+    </section>
   );
 };
 
