7938e92b649026debef1d7c5f6db4127a978ce1d
Abdukarim <abduhashimov04@gmail.com>
2023-05-18 11:34:42 +0300

    feedback-started

--- client/src/components/Aside/Aside.tsx
+++ client/src/components/Aside/Aside.tsx
@@ -5,7 +5,11 @@ import "./aside.css";
 const Aside = () => {
   return (
       <aside>
-
+        <span>Обратная связь</span>
+        <input placeholder='Ваш Email' type="text"/>
+        <input placeholder='Ваш Username' type="text"/>
+        <input placeholder='Ваше сообщение' type="text"/>
+        <button>Отправить</button>
       </aside>
   );
 };
