8bde651a6b1f1a1b04ac83c18a43ac0bfd5aa648
Abdukarim <abduhashimov04@gmail.com>
2023-05-18 11:25:18 +0300

    section-started

--- client/src/components/Aside/Aside.tsx
+++ client/src/components/Aside/Aside.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./aside.css";
 
 const Aside = () => {
-  return <aside></aside>;
+  return (
+      <aside>
+
+      </aside>
+  );
 };
 
 export default Aside;
--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import './section.css'
 
 const Section = () => {
-  return <div></div>;
+  return (
+      <section className='section'>
+          <div className="section-header">
+              title
+          </div>
+          <div className="section-article">
+              <div className="section-article-left">
+                <img src="https://diamondelectric.ru/images/1853/1852897/pylsometr_s_fynkciei_shagomera_mad_wave_pulsewatch_1.jpg" alt=""/>
+              </div>
+              <div className="section-article-right">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta est et eveniet exercitationem id ipsum maiores optio perspiciatis reprehenderit! Ab consectetur cum doloremque ducimus facere ipsam odio quae quod.
+              </div>
+          </div>
+          <div className="section-footer">
+              footer
+          </div>
+      </section>
+  );
 };
 
 export default Section;
